fec6b9f08498723151d3c70d5a0b7b2f7a6d1c0a
IKholopov <kholopov96@gmail.com>
2017-07-08 13:37:42 +0300

    MapParser, настройки на 25 SDK

--- app/src/main/java/com/team4/yamblz/timetogo/data/MapParser.java
+++ app/src/main/java/com/team4/yamblz/timetogo/data/MapParser.java
@@ -1,8 +1,13 @@
 package com.team4.yamblz.timetogo.data;
 
-/**
- * Created by igor on 7/8/17.
- */
+import org.json.JSONException;
+
+import java.util.Calendar;
 
 public interface MapParser {
+    /*
+     * Делает запрос (блокирующий) о времени, необходимого, чтобы добраться до события.
+     * Возвращает либо календарь, указывающий на дату-время прибытия, либо null в случае ошибки
+     */
+    Calendar GetTimeToEvent(double latitude, double longitude, RouteMode mode) throws JSONException;
 }
